22ba89b9958d4601350cf4814a16398fe21a6046
Codinggenie2004 <112333409+Codinggenie2004@users.noreply.github.com>
2024-01-27 15:45:58 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,53 @@
+// Get modal element
+var modal = document.getElementById('signInModal');
+// Get open modal button
+var signInBtn = document.getElementById('signInBtn');
+// Get close button
+var closeBtn = document.getElementsByClassName('close')[0];
+
+// Listen for open click
+signInBtn.addEventListener('click', openModal);
+// Listen for close click
+closeBtn.addEventListener('click', closeModal);
+// Listen for outside click
+window.addEventListener('click', outsideClick);
+
+// Function to open modal
+function openModal(){
+  modal.style.display = 'block';
+}
+
+// Function to close modal
+function closeModal(){
+  modal.style.display = 'none';
+}
+
+// Function to close modal if outside click
+function outsideClick(e){
+  if(e.target == modal){
+    modal.style.display = 'none';
+  }
+}
+let slideIndex = 0;
+  showSlides(slideIndex);
+
+  // Next/previous controls
+  function moveSlide(n) {
+    showSlides(slideIndex += n);
+  }
+
+  // Thumbnail image controls
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+
+  function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("carousel-images")[0].getElementsByTagName("img");
+    if (n >= slides.length) {slideIndex = 0}
+    if (n < 0) {slideIndex = slides.length - 1}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slides[slideIndex].style.display = "block";
+  }
